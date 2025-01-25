618da9eca333289b94e4b1a136a2018e39690313
Rohit Verma <28vrohit@gmail.com>
2025-01-25 13:02:45 +0530

    add route

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
+const router = require("./route");
 dotenv.config();
 
 const app = express();
 
 app.use(bodyParser.json());
 
-//app.use("/api", userRoutes);
+app.use("/api", router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
