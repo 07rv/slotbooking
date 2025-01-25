18dcdade0da0ec7dc5fb57ba99eb7b07fafb458b
Rohit Verma <28vrohit@gmail.com>
2025-01-25 16:23:55 +0530

    .

---
+++ models/client.ts
@@ -0,0 +1,12 @@
+class Client {
+  _id: number;
+  name: string;
+  phone_number: string;
+  alt_number: string;
+  address: Address; //Address
+}
+
+class Address {
+  street: string;
+  floor: string;
+}
